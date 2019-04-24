e2e0c0d1104621c436a546d8e69f3e70f5059bfe
Home <jamesdordoy@gmail.com>
2019-04-24 20:41:08 +0100

    Added trait

--- src/resources/assets/app.js
+++ src/resources/assets/app.js
@@ -1,5 +1,3 @@
-window.packageNameEvent = new Vue();
-
 Vue.component('vue-table',                  require('./components/Table.vue'));
 Vue.component('data-table',                  require('./components/DataTable.vue'));
 // Vue.component('data-table-anchor-cell',       require('./components/generic/data-table/generic/DataTableAnchorCell.vue'));
