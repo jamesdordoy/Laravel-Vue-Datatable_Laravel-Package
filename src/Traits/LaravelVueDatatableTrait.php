fbe04639a5ec1b4575801fcead23fd861f8ede69
Home <jamesdordoy@gmail.com>
2019-06-14 20:41:38 +0100

    Tidying up and updating README.

--- src/Traits/LaravelVueDatatableTrait.php
+++ src/Traits/LaravelVueDatatableTrait.php
@@ -14,33 +14,24 @@ trait LaravelVueDatatableTrait
         if (isset(array_keys($columns)[$column])) {
             $query = $query->orderBy(array_keys($columns)[$column], $orderBy);
         } else {
-
             $defaultOrderBy = config('laravel-vue-datatables.default_order_by');
-
             if (is_null($defaultOrderBy)) {
                 $defaultOrderBy = 'id';
             }
-
             $query = $query->orderBy($defaultOrderBy, $orderBy);
         }
 
         if ($searchValue) {
             $query->where(function($query) use ($searchValue, $columns) {
-
+                $searchTerm = config('laravel-vue-datatables.models.search_term');
                 $first = true;
-
                 if (count($columns)) {
                     foreach ($columns as $key => $column) {
                         if ($first) {
-    
-                            $searchTerm = config('laravel-vue-datatables.models.search_term');
-
                             if (isset($column[$searchTerm])) {
                                 $query->where($key, 'like', '%' . $searchValue . '%');
                             }
-
-                            $first = false;   
-                            
+                            $first = false;
                         } else {
                             if ($column[$searchTerm]) {
                                 $query->orWhere($key, 'like', '%' . $searchValue . '%');
