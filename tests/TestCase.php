97ce3907f1397d8aa3eee5459d584b4d2ba7960b
James Dordoy <jamesdordoy@gmail.com>
2020-01-06 17:00:38 +0000

    Setting up github actions for testing.

---
+++ tests/TestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+
+class TestCase extends Orchestra\Testbench\TestCase
+{
+    //
+
+    public function test_is_working()
+    {
+        $this->assertTrue(true);
+    }
+}
